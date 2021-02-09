ee3db6afc6f1522f2927e8e11dda2d27f53184bf
Denis-Michael Lux <denismichael.lux@scoop-software.de>
2021-02-09 21:37:08 +0100

    refactor: adds linter fixes

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,31 +1,31 @@
-import * as React from 'react';
+import * as React from "react"
 
-import { StyleSheet, View, Text } from 'react-native';
-import NavigationStateMachine from 'react-native-navigation-state-machine';
+import { StyleSheet, View, Text } from "react-native"
+import NavigationStateMachine from "react-native-navigation-state-machine"
 
 export default function App() {
-  const [result, setResult] = React.useState<number | undefined>();
+  const [result, setResult] = React.useState<number | undefined>()
 
   React.useEffect(() => {
-    NavigationStateMachine.multiply(3, 7).then(setResult);
-  }, []);
+    NavigationStateMachine.multiply(3, 7).then(setResult)
+  }, [])
 
   return (
     <View style={styles.container}>
       <Text>Result: {result}</Text>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   box: {
     width: 60,
     height: 60,
     marginVertical: 20,
   },
-});
+})
